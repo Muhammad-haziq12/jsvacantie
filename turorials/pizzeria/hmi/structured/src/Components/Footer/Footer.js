9680786d22b8f0800596afd3b2509e705a1c354f
Muhammad-haziq12 <you@example.com>
2022-11-04 12:42:25 +0100

    'routing-hmi'

--- turorials/pizzeria/hmi/structured/src/Components/Footer/Footer.js
+++ turorials/pizzeria/hmi/structured/src/Components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import image1 from '../../img/pizza.jpg';
 import image2 from '../../img/js-logo.png';
+import Navigate  from '../Router/Navigate';
 
 const Footer = () =>{
     const footer = document.querySelector('footer');
@@ -8,15 +9,25 @@ const Footer = () =>{
   </h1>`;
 
 
-renderSmallImage(footer, image1);
+renderSmallImage(footer, image1, 'chessePizza');
 renderSmallImage(footer, image2);
+attachOnPizzaClick();
 };
 
 export default Footer;
 
-function renderSmallImage(wrapper, url){
+function renderSmallImage(wrapper, url, id){
 const image = new Image();
 image.src = url;
 image.height = 50;
+if (id) image.id = id;
 wrapper.appendChild(image);
 }
+
+function attachOnPizzaClick() {
+  const pizza = document.querySelector('#chessePizza');
+  pizza.addEventListener('click', () =>{
+    Navigate('/');
+
+  })
+};
